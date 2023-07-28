0c5ee86cc559757e39ed233d89df6ce36931bf9b
nrjxanl <shwnn06@naver.com>
2023-07-29 00:22:18 +0900

    edit files

--- script.js
+++ script.js
@@ -41,61 +41,31 @@ function fixturesU15() {
 }
 
 //엠블럼 삽입
-$("td[class*='sefcH']").before("<td>서울 이랜드 FC</td><td><img src='./files/seouleland_s.png'></td>");
-$("td[class*='ganH']").before("<td>강원 FC</td><td><img src='./files/gangwonfc_s.png'></td>");
-$("td[class*='gyeH']").before("<td>경남 FC</td><td><img src='./files/gyeongnamfc_s.png'></td>");
-$("td[class*='gwaH']").before("<td>광주 FC</td><td><img src='./files/gwangjufc_s.png'></td>");
-$("td[class*='gimH']").before("<td>김천 상무 FC</td><td><img src='./files/gimcheonsangmu_s.png'></td>");
-$("td[class*='gfcH']").before("<td>김포 FC</td><td><img src='./files/gimpofc_s.png'></td>");
-$("td[class*='daeH']").before("<td>대구 FC</td><td><img src='./files/daegufc_s.png'></td>");
-$("td[class*='dhfcH']").before("<td>대전 하나 시티즌</td><td><img src='./files/daejeon_s.png'></td>");
-$("td[class*='busH']").before("<td>부산 아이파크</td><td><img src='./files/busanipark_s.png'></td>");
-$("td[class*='bucH']").before("<td>부천 FC 1995</td><td><img src='./files/bucheonfc_s.png'></td>");
-$("td[class*='fcsH']").before("<td>FC 서울</td><td><img src='./files/fcseoul_s.png'></td>");
-$("td[class*='sfcH']").before("<td>성남 FC</td><td><img src='./files/seongnamfc_s.png'></td>");
-$("td[class*='ssbH']").before("<td>수원 삼성 블루윙즈</td><td><img src='./files/suwonsamsung_s.png'></td>");
-$("td[class*='suwH']").before("<td>수원 FC</td><td><img src='./files/suwonfc_s.png'></td>");
-$("td[class*='ansH']").before("<td>안산 그리너스</td><td><img src='./files/ansangreeners_s.png'></td>");
-$("td[class*='anyH']").before("<td>FC 안양</td><td><img src='./files/fcanyang_s.png'></td>");
-$("td[class*='ulsH']").before("<td>울산 현대 축구단</td><td><img src='./files/ulsanhyundai_s.png'></td>");
-$("td[class*='incH']").before("<td>인천 유나이티드</td><td><img src='./files/incheonutd_s.png'></td>");
-$("td[class*='jdfcH']").before("<td>전남 드래곤즈</td><td><img src='./files/jeonnamdragons_s.png'></td>");
-$("td[class*='jeoH']").before("<td>전북 현대 모터스</td><td><img src='./files/jeonbukhyundai_s.png'></td>");
-$("td[class*='jejH']").before("<td>제주 유나이티드</td><td><img src='./files/jejuutd_s.png'></td>");
-$("td[class*='cafcH']").before("<td>충남아산 FC</td><td><img src='./files/chungnamasan_s.png'></td>");
-$("td[class*='pohH']").before("<td>포항 스틸러스</td><td><img src='./files/pohangsteelers_s.png'></td>");
-$("td[class*='ccfcH']").before("<td>천안 시티 FC</td><td><img src='./files/cheonancity_s.png'></td>");
-$("td[class*='chfcH']").before("<td>충북청주 FC</td><td><img src='./files/chungbukcheongju_s.png'></td>");
+$("td[class='sefc']").after("<td style='opacity: 100% !important;'><img src='./files/seouleland_s.png'></td><td style='opacity: 100% !important;'>서울 이랜드 FC</td>");
+$("td[class='gan']").after("<td><img src='./files/gangwonfc_s.png'></td><td>강원 FC</td>");
+$("td[class='gye']").after("<td><img src='./files/gyeongnamfc_s.png'></td><td>경남 FC</td>");
+$("td[class='gwa']").after("<td><img src='./files/gwangjufc_s.png'></td><td>광주 FC</td>");
+$("td[class='gim']").after("<td><img src='./files/gimcheonsangmu_s.png'></td><td>김천 상무 FC</td>");
+$("td[class='gfc']").after("<td><img src='./files/gimpofc_s.png'></td><td>김포 FC</td>");
+$("td[class='dae']").after("<td><img src='./files/daegufc_s.png'></td><td>대구 FC</td>");
+$("td[class='dhfc']").after("<td><img src='./files/daejeon_s.png'></td><td>대전 하나 시티즌</td>");
+$("td[class='bus']").after("<td><img src='./files/busanipark_s.png'></td><td>부산 아이파크</td>");
+$("td[class='buc']").after("<td><img src='./files/bucheonfc_s.png'></td><td>부천 FC 1995</td>");
+$("td[class='fcs']").after("<td><img src='./files/fcseoul_s.png'></td><td>FC 서울</td>");
+$("td[class='sfc']").after("<td><img src='./files/seongnamfc_s.png'></td><td>성남 FC</td>");
+$("td[class='ssb']").after("<td><img src='./files/suwonsamsung_s.png'></td><td>수원 삼성 블루윙즈</td>");
+$("td[class='suw']").after("<td><img src='./files/suwonfc_s.png'></td><td>수원 FC</td>");
+$("td[class='ans']").after("<td><img src='./files/ansangreeners_s.png'></td><td>안산 그리너스</td>");
+$("td[class='any']").after("<td><img src='./files/fcanyang_s.png'></td><td>FC 안양</td>");
+$("td[class='uls']").after("<td><img src='./files/ulsanhyundai_s.png'></td><td>울산 현대 축구단</td>");
+$("td[class='inc']").after("<td><img src='./files/incheonutd_s.png'></td><td>인천 유나이티드</td>");
+$("td[class='jdfc']").after("<td><img src='./files/jeonnamdragons_s.png'></td><td>전남 드래곤즈</td>");
+$("td[class='jeo']").after("<td><img src='./files/jeonbukhyundai_s.png'></td><td>전북 현대 모터스</td>");
+$("td[class='jej']").after("<td><img src='./files/jejuutd_s.png'></td><td>제주 유나이티드</td>");
+$("td[class='cafc']").after("<td><img src='./files/chungnamasan_s.png'></td><td>충남아산 FC</td>");
+$("td[class='poh']").after("<td><img src='./files/pohangsteelers_s.png'></td><td>포항 스틸러스</td>");
+$("td[class='ccfc']").after("<td><img src='./files/cheonancity_s.png'></td><td>천안 시티 FC</td>");
+$("td[class='chfc']").after("<td><img src='./files/chungbukcheongju_s.png'></td><td>충북청주 FC</td>");
 
-$("td[class*='sefcA']").after("<td><img src='./files/seouleland_s.png'></td><td>서울 이랜드 FC</td>");
-$("td[class*='ganA']").after("<td><img src='./files/gangwonfc_s.png'></td><td>강원 FC</td>");
-$("td[class*='gyeA']").after("<td><img src='./files/gyeongnamfc_s.png'></td><td>경남 FC</td>");
-$("td[class*='gwaA']").after("<td><img src='./files/gwangjufc_s.png'></td><td>광주 FC</td>");
-$("td[class*='gimA']").after("<td><img src='./files/gimcheonsangmu_s.png'></td><td>김천 상무 FC</td>");
-$("td[class*='gfcA']").after("<td><img src='./files/gimpofc_s.png'></td><td>김포 FC</td>");
-$("td[class*='daeA']").after("<td><img src='./files/daegufc_s.png'></td><td>대구 FC</td>");
-$("td[class*='dhfcA']").after("<td><img src='./files/daejeon_s.png'></td><td>대전 하나 시티즌</td>");
-$("td[class*='busA']").after("<td><img src='./files/busanipark_s.png'></td><td>부산 아이파크</td>");
-$("td[class*='bucA']").after("<td><img src='./files/bucheonfc_s.png'></td><td>부천 FC 1995</td>");
-$("td[class*='fcsA']").after("<td><img src='./files/fcseoul_s.png'></td><td>FC 서울</td>");
-$("td[class*='sfcA']").after("<td><img src='./files/seongnamfc_s.png'></td><td>성남 FC</td>");
-$("td[class*='ssbA']").after("<td><img src='./files/suwonsamsung_s.png'></td><td>수원 삼성 블루윙즈</td>");
-$("td[class*='suwA']").after("<td><img src='./files/suwonfc_s.png'></td><td>수원 FC</td>");
-$("td[class*='ansA']").after("<td><img src='./files/ansangreeners_s.png'></td><td>안산 그리너스</td>");
-$("td[class*='anyA']").after("<td><img src='./files/fcanyang_s.png'></td><td>FC 안양</td>");
-$("td[class*='ulsA']").after("<td><img src='./files/ulsanhyundai_s.png'></td><td>울산 현대 축구단</td>");
-$("td[class*='incA']").after("<td><img src='./files/incheonutd_s.png'></td><td>인천 유나이티드</td>");
-$("td[class*='jdfcA']").after("<td><img src='./files/jeonnamdragons_s.png'></td><td>전남 드래곤즈</td>");
-$("td[class*='jeoA']").after("<td><img src='./files/jeonbukhyundai_s.png'></td><td>전북 현대 모터스</td>");
-$("td[class*='jejA']").after("<td><img src='./files/jejuutd_s.png'></td><td>제주 유나이티드</td>");
-$("td[class*='cafcA']").after("<td><img src='./files/chungnamasan_s.png'></td><td>충남아산 FC</td>");
-$("td[class*='pohA']").after("<td><img src='./files/pohangsteelers_s.png'></td><td>포항 스틸러스</td>");
-$("td[class*='ccfcA']").after("<td><img src='./files/cheonancity_s.png'></td><td>천안 시티 FC</td>");
-$("td[class*='chfcA']").after("<td><img src='./files/chungbukcheongju_s.png'></td><td>충북청주 FC</td>");
-
-// 점수 색
-
-$("td[class='w']").css("background", "#174fff")
-$("td[class='d']").css("background", "#00000080")
-$("td[class='l']").css("background", "#f00")
-$("td[class='y']").css("color", "#000831")
+$("td[class='kl40']").after("<td rowspan='2'><img src='./files/kleague40_s.png'></td>");
+$("td[class='fa']").after("<td rowspan='2'><img src='./files/facup_s.png'></td>");
