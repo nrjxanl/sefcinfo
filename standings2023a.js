84080728efa67e4c38251470d2c91c3a915c7164
nrjxanl <shwnn06@naver.com>
2023-10-23 17:47:07 +0900

    edit files

--- standings2023a.js
+++ standings2023a.js
@@ -1,12 +1,12 @@
 data = {
-    "team": ["부산", "김천", "김포", "부천", "경남", "전남", "충북청주", "안양", "성남", "충남아산", "서울E", "안산", "천안"],
-    "pts": ["63", "61", "56", "53", "49", "47", "47", "44", "41", "36", "35", "22", "21"],
-    "w": ["18", "19", "15", "15", "13", "14", "12", "12", "10", "10", "10", "5", "4"],
-    "d": ["9", "4", "11", "8", "10", "5", "11", "8", "11", "6", "5", "7", "9"],
-    "l": ["5", "9", "6", "10", "9", "13", "8", "12", "12", "16", "18", "20", "19"],
-    "goalS": ["45", "60", "38", "39", "47", "47", "32", "49", "40", "33", "36", "31", "29"],
-    "goalC": ["23", "33", "21", "31", "38", "50", "34", "47", "45", "41", "48", "58", "57"],
-    "diff": ["22", "27", "17", "8", "9", "-3", "-2", "2", "-5", "-8", "-12", "-27", "-28"],
+    "team": ["부산", "김천", "김포", "부천", "경남", "충북청주", "전남", "안양", "성남", "충남아산", "서울E", "천안", "안산"],
+    "pts": ["66", "64", "59", "54", "50", "48", "47", "45", "41", "39", "35", "24", "22"],
+    "w": ["19", "20", "16", "15", "13", "12", "14", "12", "10", "11", "10", "5", "5"],
+    "d": ["9", "4", "11", "9", "11", "12", "5", "9", "11", "6", "5", "9", "7"],
+    "l": ["5", "9", "6", "10", "9", "9", "14", "12", "13", "17", "18", "19", "21"],
+    "goalS": ["47", "67", "40", "40", "48", "35", "48", "50", "41", "37", "36", "32", "34"],
+    "goalC": ["24", "36", "22", "32", "39", "38", "52", "48", "48", "45", "48", "58", "65"],
+    "diff": ["23", "31", "18", "8", "9", "-3", "-4", "2", "-7", "-8", "-12", "-26", "-31"],
 }
 
 teamList = Object.values(data.team);
@@ -18,17 +18,9 @@ goalSList = Object.values(data.goalS);
 goalCList = Object.values(data.goalC);
 diffList = Object.values(data.diff);
 
-for (i = 0; i < $("#standingsA > div > table > tbody > tr").length; i++) {
-    $("#standingsA").find("tr:nth-of-type(" + (i + 1) + ") > td:nth-of-type(1)").text(i + 1);
-    $("#standingsA").find("tr:nth-of-type(" + (i + 1) + ") > td:nth-of-type(3)").text(teamList[i]);
-    $("#standingsA").find("tr:nth-of-type(" + (i + 1) + ") > td:nth-of-type(4)").text(ptsList[i]);
-    $("#standingsA").find("tr:nth-of-type(" + (i + 1) + ") > td:nth-of-type(5)").text(wList[i]);
-    $("#standingsA").find("tr:nth-of-type(" + (i + 1) + ") > td:nth-of-type(6)").text(dList[i]);
-    $("#standingsA").find("tr:nth-of-type(" + (i + 1) + ") > td:nth-of-type(7)").text(lList[i]);
-    $("#standingsA").find("tr:nth-of-type(" + (i + 1) + ") > td:nth-of-type(8)").text(goalSList[i]);
-    $("#standingsA").find("tr:nth-of-type(" + (i + 1) + ") > td:nth-of-type(9)").text(goalCList[i]);
-    $("#standingsA").find("tr:nth-of-type(" + (i + 1) + ") > td:nth-of-type(10)").text(diffList[i]);
+j = teamList.indexOf("서울E");
 
+for (i = 0; i < teamList.length; i++) {
     emblem = 
         teamList[i] == "서울E" ? "seouleland_s" :
         teamList[i] == "울산" ? "ulsanhyundai2016_s" :
@@ -56,10 +48,47 @@ for (i = 0; i < $("#standingsA > div > table > tbody > tr").length; i++) {
         teamList[i] == "부천" ? "bucheonfc_s" :
         teamList[i] == "안산" ? "ansangreeners_s" :
         "dummy";
+        $("#standingsA").find("tr:nth-of-type(" + (i + 1) + ") > td:nth-of-type(1)").text(i + 1);
+        $("#standingsA").find("tr:nth-of-type(" + (i + 1) + ") > td:nth-of-type(2)").html("<img src='./files/" + emblem + ".png'>");
+        $("#standingsA").find("tr:nth-of-type(" + (i + 1) + ") > td:nth-of-type(3)").text(teamList[i]);
+        $("#standingsA").find("tr:nth-of-type(" + (i + 1) + ") > td:nth-of-type(4)").text(ptsList[i]);
+        $("#standingsA").find("tr:nth-of-type(" + (i + 1) + ") > td:nth-of-type(5)").text(wList[i]);
+        $("#standingsA").find("tr:nth-of-type(" + (i + 1) + ") > td:nth-of-type(6)").text(dList[i]);
+        $("#standingsA").find("tr:nth-of-type(" + (i + 1) + ") > td:nth-of-type(7)").text(lList[i]);
+        $("#standingsA").find("tr:nth-of-type(" + (i + 1) + ") > td:nth-of-type(8)").text(goalSList[i]);
+        $("#standingsA").find("tr:nth-of-type(" + (i + 1) + ") > td:nth-of-type(9)").text(goalCList[i]);
+        $("#standingsA").find("tr:nth-of-type(" + (i + 1) + ") > td:nth-of-type(10)").text(diffList[i]);
 
-    $("#standingsA").find("tr:nth-of-type(" + (i + 1) + ") > td:nth-of-type(2)").html("<img src='./files/" + emblem + ".png'>");
+    if (j == 0) {
+        if (i >= j && i <= (j + 2)) {
+            $("#standingsHome").find("tr:nth-of-type(" + (i + 1) + ") > td:nth-of-type(1)").text(i + 1);
+            $("#standingsHome").find("tr:nth-of-type(" + (i + 1) + ") > td:nth-of-type(2)").html("<img src='./files/" + emblem + ".png'>");
+            $("#standingsHome").find("tr:nth-of-type(" + (i + 1) + ") > td:nth-of-type(3)").text(teamList[i]);
+            $("#standingsHome").find("tr:nth-of-type(" + (i + 1) + ") > td:nth-of-type(4)").text(ptsList[i]);
+            $("#standingsHome").find("tr:nth-of-type(" + (i + 1) + ") > td:nth-of-type(5)").text(goalSList[i]);
+            $("#standingsHome").find("tbody > tr:nth-of-type(1)").attr("class", "sefc");
+        };
+    } else if (j == (teamList.length - 1)) {
+        if (i >= (j - 2) && i <= j) {
+            $("#standingsHome").find("tr:nth-of-type(" + (i - (j - 3)) + ") > td:nth-of-type(1)").text(i + 1);
+            $("#standingsHome").find("tr:nth-of-type(" + (i - (j - 3)) + ") > td:nth-of-type(2)").html("<img src='./files/" + emblem + ".png'>");
+            $("#standingsHome").find("tr:nth-of-type(" + (i - (j - 3)) + ") > td:nth-of-type(3)").text(teamList[i]);
+            $("#standingsHome").find("tr:nth-of-type(" + (i - (j - 3)) + ") > td:nth-of-type(4)").text(ptsList[i]);
+            $("#standingsHome").find("tr:nth-of-type(" + (i - (j - 3)) + ") > td:nth-of-type(5)").text(goalSList[i]);
+            $("#standingsHome").find("tbody > tr:nth-of-type(3)").attr("class", "sefc");
+        };
+    } else {
+        if (i >= (j - 1) && i <= (j + 1)) {
+            $("#standingsHome").find("tr:nth-of-type(" + (i - (j - 2)) + ") > td:nth-of-type(1)").text(i + 1);
+            $("#standingsHome").find("tr:nth-of-type(" + (i - (j - 2)) + ") > td:nth-of-type(2)").html("<img src='./files/" + emblem + ".png'>");
+            $("#standingsHome").find("tr:nth-of-type(" + (i - (j - 2)) + ") > td:nth-of-type(3)").text(teamList[i]);
+            $("#standingsHome").find("tr:nth-of-type(" + (i - (j - 2)) + ") > td:nth-of-type(4)").text(ptsList[i]);
+            $("#standingsHome").find("tr:nth-of-type(" + (i - (j - 2)) + ") > td:nth-of-type(5)").text(goalSList[i]);
+            $("#standingsHome").find("tbody > tr:nth-of-type(2)").attr("class", "sefc");
+        };
+    };
 
     if (teamList[i] == "서울E") {
-        $("#standingsA").find("tr:nth-of-type(" + (i + 1) + ")").attr("class", "sefc")
+        $("#standingsA").find("tbody > tr:nth-of-type(" + (i + 1) + ")").attr("class", "sefc");
     };
 };
