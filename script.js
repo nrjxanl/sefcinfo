74277440be8b545e431ea23ccf1eb6962241ae02
nrjxanl <shwnn06@naver.com>
2023-10-30 23:59:20 +0900

    edit files

--- script.js
+++ script.js
@@ -94,18 +94,21 @@ function playerA() {
     document.getElementById("playerA").style.display = "block";
     document.getElementById("playerU18").style.display = "none";
     document.getElementById("playerU15").style.display = "none";
+    document.getElementById("playerAllTime").style.display = "none";
 };
 
 function playerU18() {
     document.getElementById("playerA").style.display = "none";
     document.getElementById("playerU18").style.display = "block";
     document.getElementById("playerU15").style.display = "none";
+    document.getElementById("playerAllTime").style.display = "none";
 };
 
 function playerU15() {
     document.getElementById("playerA").style.display = "none";
     document.getElementById("playerU18").style.display = "none";
     document.getElementById("playerU15").style.display = "block";
+    document.getElementById("playerAllTime").style.display = "none";
 };
 
 // 일정 창 전환
