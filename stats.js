8377d15d91cc9183da5b217c8edb8510151efd75
nrjxanl <gwcom112@gmail.com>
2025-12-31 16:29:34 +0900

    기록 페이지 선수 누르면 선수 프로필로 이동하도록 수정

--- stats.js
+++ stats.js
@@ -130,7 +130,12 @@ function render(dataA, dataU18, dataU15, playerA, playerU18, playerU15, playerNu
         currSortIdx = idx + 1;
 
         sort(currSortIdx);
-    })
+    });
+
+    // 클릭 시 선수 프로필로 이동
+    $('#stats > .stats > table > tbody > tr').click(function() {
+        window.location.href = `./player?${$(this).attr('class')}`;
+    });
 }
 
 // 스탯 계산
